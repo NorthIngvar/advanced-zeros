49ff266e001c8b14ea14fa50562d1b86028bde97
Killjoy4 <dedovich1.17@gmail.com>
2018-09-30 01:30:11 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var result = Number.MAX_VALUE, 
+    array = [],
+    count, copyNumber,
+    copyBase = base;
+       
+  for (let a = 2; a <= base; a++) { 
+    let temporary = 0; 
+    while (base % a === 0 && base > 0) { 
+      temporary++; 
+      base /= a; 
+    }
+
+    if (temporary > 0) {
+      array[a] = temporary;
+    }
+    
+  }
+  for  (let a = 2; a <= copyBase; a++) {
+    if (array[a] != null) {
+      count = 0;
+      copyNumber = number;
+      while (copyNumber  / a > 0) {
+        count += Math.floor(copyNumber / a);
+        copyNumber /= a;
+      }
+      result = Math.min(result, Math.floor(count / array[a]));
+    }
+  } 
+  return result;
 }
